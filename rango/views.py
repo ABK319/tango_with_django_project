e2965a377b965cbceb1520de24bd099c2e3bdfb2
Abhishek Kulkarni <2253426K@student.gla.ac.uk>
2019-02-08 11:23:00 +0000

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -12,23 +12,31 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
 
-def visitor_cookie_handler(request,response):
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
-    
-    visits = int(request.COOKIES.get("visits","1"))
+def visitor_cookie_handler(request):
+   
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now())) 
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()) )
 
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
     
     if (datetime.now() - last_visit_time).seconds > 0:
         visits = visits + 1
-        response.set_cookie("last_visit", last_visit_cookie)
+       
+        request.session['last_visit'] = str(datetime.now())
     else:
+        visits = 1
+        
+        request.session['last_visit'] = last_visit_cookie
+   
+    request.session['visits'] = visits
 
-        response.set_cookie("last_visit", last_visit_cookie)
-
-    response.set_cookie("visits", visits)
         
 @login_required
 def restricted(request):
@@ -145,16 +153,22 @@ def add_page(request, category_name_slug):
     return render(request, "rango/add_page.html", context_dict)
 
 def index(request):
-   
+    
+    request.session.set_test_cookie()
+    
     category_list = Category.objects.order_by('-likes')[:5]
     
     page_list = Page.objects.order_by('-views')[:5]
     
     context_dict = {'categories': category_list, 'pages': page_list}
     
+    visitor_cookie_handler(request)
+    
+    context_dict['visits'] = request.session['visits']
+    
+    print(request.session['visits'])
+    
     response = render(request, 'rango/index.html', context=context_dict)
-
-    visitor_cookie_handler(request, response)
     
     return response
 
